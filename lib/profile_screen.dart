7bb4c116bb69ac06fa94f8430a48f9d4a7037885
Rustem <r132420@gmail.com>
2024-04-21 23:26:00 +0300

    + 1. Керлинго. Создание проекта, начало главного окна. (profile_screen.dart)

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -5,6 +5,63 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Профиль"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text("Save"),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 8, 20, 24),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(90),
+                  color: Colors.green,
+                ),
+                height: 80,
+                width: 80,
+                child: Center(
+                  child: Text(
+                    "Edit",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(height: 24),
+              Text("Мои награды"),
+              SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("🥇", style: TextStyle(fontSize: 32)),
+                  SizedBox(width: 16),
+                  Text("🥇", style: TextStyle(fontSize: 32)),
+                  SizedBox(width: 16),
+                  Text("🥉", style: TextStyle(fontSize: 32)),
+                  SizedBox(width: 16),
+                  Text("🥈", style: TextStyle(fontSize: 32)),
+                  SizedBox(width: 16),
+                  Text("🥉", style: TextStyle(fontSize: 32)),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
